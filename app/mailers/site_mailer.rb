d131adced9173058cb8da2cb6c4cfd5814e976c3
German Grandson <ganieto1@gmail.com>
2017-03-14 12:36:24 -0400

    smtp server fix

--- app/mailers/site_mailer.rb
+++ app/mailers/site_mailer.rb
@@ -2,7 +2,7 @@ class SiteMailer < ActionMailer::Base
 
   def send_message(name, email, message)
 
-    mail to: 'ganieto1@gmail.com', subject: "Site message from #{name}", from: email, body: message
+    mail to: 'ganieto1@gmail.com', subject: "Site message from #{name} #{email}", from: email, body: message
 
   end
 
